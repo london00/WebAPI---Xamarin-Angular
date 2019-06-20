2d9bf56be3a52ba158f16fba4aa3a0083f276fdf
Geiser Aragón Cejas <isc.geiser.aragon.cejas@outlook.com>
2019-06-20 11:25:51 -0500

    Add "Make" and "Model" models to project.

---
+++ UdemyDotNetCoreAngular/Models/Make.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace UdemyDotNetCoreAngular.Models
+{
+    public class Make
+    {
+        public Make()
+        {
+            Models = new Collection<Model>();
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Model> Models { get; set; }
+    }
+}
