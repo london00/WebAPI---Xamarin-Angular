60ee6f4b4f174359e551aca95197886d54e9b54a
Geiser Aragón Cejas <isc.geiser.aragon.cejas@outlook.com>
2019-06-20 12:44:25 -0500

    Add conection string to db context.

---
+++ UdemyDotNetCoreAngular/Domain/VegaDBContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace UdemyDotNetCoreAngular.Domain
+{
+    public class VegaDBContext : DbContext
+    {
+        public VegaDBContext(DbContextOptions<VegaDBContext> dbContext): base(dbContext)
+        {
+
+        }
+    }
+}
