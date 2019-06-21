051712bf00afd1c594c3f110b1e2835500841015
Geiser Aragón Cejas <isc.geiser.aragon.cejas@outlook.com>
2019-06-21 12:53:12 -0500

    Populate "Make"  dropdown list

--- UdemyDotNetCoreAngular/ClientApp/src/app/vehicle/vehicle.component.ts
+++ UdemyDotNetCoreAngular/ClientApp/src/app/vehicle/vehicle.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { debug } from 'util';
 
 @Component({
   selector: 'app-vehicle',
@@ -6,4 +8,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./vehicle.component.css']
 })
 export class VehicleComponent {
+  public makes: MakeDTO[];
+
+  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+    http.get<MakeDTO[]>(baseUrl + 'api/Makes/GetMakes').subscribe(result => {
+      this.makes = result;
+    }, error => console.error(error));
+  }
+}
+
+interface MakeDTO {
+  Id: number;
+  Name: string;
+  Models: ModelDTO[];
+}
+
+interface ModelDTO {
+  Id: number;
+  Name: string;
+  MakeId: number;
 }
