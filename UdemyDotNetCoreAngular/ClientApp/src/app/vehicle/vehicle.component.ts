13766729f1a7ca5a707a7fc2f0760a8e0ad6667a
Geiser Aragón Cejas <isc.geiser.aragon.cejas@outlook.com>
2019-06-21 12:01:20 -0500

    Create vehicle component. Currently has a dummy template.

---
+++ UdemyDotNetCoreAngular/ClientApp/src/app/vehicle/vehicle.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vehicle',
+  templateUrl: './vehicle.component.html',
+  styleUrls: ['./vehicle.component.css']
+})
+export class VehicleComponent {
+}
