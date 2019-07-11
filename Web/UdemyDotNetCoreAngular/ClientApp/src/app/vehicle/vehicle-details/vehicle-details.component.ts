08f1a9c4a315b92f6377095f66e49712a7af89e0
Geiser Aragón Cejas <isc.geiser.aragon.cejas@outlook.com>
2019-07-11 15:57:45 -0500

    Add vehicle details component.

--- Web/UdemyDotNetCoreAngular/ClientApp/src/app/vehicle/vehicle-details/vehicle-details.component.ts
+++ Web/UdemyDotNetCoreAngular/ClientApp/src/app/vehicle/vehicle-details/vehicle-details.component.ts
@@ -1,4 +1,9 @@
-﻿import { Component } from '@angular/core';
+import { Component } from '@angular/core';
+import { VehicleService } from '../../services/vehicle.service';
+import { ToastrService } from 'ngx-toastr';
+import { ActivatedRoute, Router } from '@angular/router';
+import { VehicleDTO } from '../../DTO/ModelContext';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
     selector: 'app-vehicle-details',
@@ -7,8 +12,45 @@
 })
 /** VehicleDetails component*/
 export class VehicleDetailsComponent {
-    /** VehicleDetails ctor */
-    constructor() {
+  private vehicle: VehicleDTO;
+  private vehicleService: VehicleService;
+  private toastyService: ToastrService;
+  private route: ActivatedRoute;
+  private router: Router;
 
+    /** VehicleDetails ctor */
+  constructor(vehicleService: VehicleService, toastyService: ToastrService, route: ActivatedRoute, router: Router) {
+    this.vehicleService = vehicleService;
+    this.toastyService = toastyService;
+    this.route = route;
+    this.router = router;
+    this.vehicle = new VehicleDTO();
+
+    route.params.subscribe(p => {
+      this.vehicle.Id = Number.parseInt(p["id"]);
+    });
+  }
+
+  ngOnInit() {
+    this.vehicleService.GetVehicle(this.vehicle.Id).subscribe(
+      (response) => {
+        this.vehicle = response;
+      },
+      (error: HttpErrorResponse) => {
+        if (error.status == 404) {
+          // Goes to home page. TODO: Create and go to 404 page error.
+          this.router.navigate(["/"]);
+        }
+      });
+  }
+
+  Delete() {
+    if (confirm("Are you sure?")) {
+      this.vehicleService.Delete(this.vehicle.Id).subscribe(
+        data => {
+          this.toastyService.success("Vehicle has been deleted", "Success!!");
+          this.router.navigate(["/"]);
+        });
     }
+  }
 }
